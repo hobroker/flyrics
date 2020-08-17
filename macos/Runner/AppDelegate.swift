7ef6bf35d7ccd3ec8a50f9364e70acff5a1a8b8f
hobroker <igor.leahu24@gmail.com>
2020-08-17 13:09:13 +0300

    Refactoring

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,11 +3,48 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+  var popover: NSPopover!
+  var statusBarItem: NSStatusItem!
+  override func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
     return true
   }
 
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print("dadaasdsad");
+  override func applicationDidFinishLaunching(_: Notification) {
+    // Create the SwiftUI view that provides the window contents.
+    let flutterViewController = FlutterViewController()
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    // Create the popover
+    let popover = NSPopover()
+    popover.contentSize = NSSize(width: 300, height: 500)
+    popover.behavior = .transient
+    popover.contentViewController = flutterViewController
+    self.popover = popover
+
+    // Create the status item
+    statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+
+    //openPopover()
+
+    if let button = statusBarItem.button {
+      button.title = "LF"
+      button.action = #selector(togglePopover(_:))
+    }
+  }
+
+  @objc func togglePopover(_ sender: AnyObject?) {
+    if let button = statusBarItem.button {
+      if popover.isShown {
+        popover.performClose(sender)
+      } else {
+        openPopover()
+      }
+    }
+  }
+
+  @objc func openPopover() {
+    if let button = statusBarItem.button {
+      popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+    }
   }
 }

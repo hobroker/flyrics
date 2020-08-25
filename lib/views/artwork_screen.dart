5fce4878e45ad2392d02cafc942467875598591e
hobroker <igor.leahu24@gmail.com>
2020-08-25 20:25:25 +0300

    Add image fade effect

--- lib/views/artwork_screen.dart
+++ lib/views/artwork_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flyrics/utils/sizing.dart';
 
 class ArtworkScreen extends StatelessWidget {
   final List<int> bytes;
+  final Color fadeColor;
 
   ArtworkScreen({
     @required this.bytes,
+    this.fadeColor = Colors.transparent,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Image.memory(this.bytes);
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            width: UI(context).headerHeight,
+            height: UI(context).headerHeight,
+            child: Image.memory(bytes),
+          ),
+          AnimatedContainer(
+            width: UI(context).headerHeight,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [fadeColor, Colors.transparent],
+                tileMode: TileMode.repeated,
+              ),
+            ),
+            duration: Duration(milliseconds: 300),
+          )
+        ],
+      ),
+    );
   }
 }

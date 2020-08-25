869816e90577dfedca24b3b346145659dcde351b
hobroker <igor.leahu24@gmail.com>
2020-08-25 20:25:25 +0300

    Add DynamicTooltip

--- lib/views/genius_redirect.dart
+++ lib/views/genius_redirect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flyrics/containers/dynamic_tooltip.dart';
 import 'package:flyrics/utils/ux.dart';
 import 'package:flyrics/views/hover_builder.dart';
 import 'package:flyrics/views/icons/genius_icon.dart';
@@ -7,7 +8,7 @@ class GeniusRedirectScreen extends StatelessWidget {
   final Color iconColor;
   final Function() openUrl;
 
-  GeniusRedirectScreen({
+  const GeniusRedirectScreen({
     Key key,
     @required this.openUrl,
     @required this.iconColor,
@@ -15,24 +16,27 @@ class GeniusRedirectScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HoverBuilder<double>(
-      value: 0.5,
-      onEnter: (double opacity) => 1.0,
-      builder: (BuildContext context, double opacity) {
-        return AnimatedOpacity(
-          opacity: opacity,
-          duration: UX.transitionDuration,
-          child: IconButton(
-            padding: EdgeInsets.all(0),
-            constraints: BoxConstraints(
-              maxWidth: 20,
-              maxHeight: 20,
+    return DynamicTooltip(
+      message: 'Open in browser',
+      child: HoverBuilder<double>(
+        value: 0.5,
+        onEnter: (double opacity) => 1.0,
+        builder: (BuildContext context, double opacity) {
+          return AnimatedOpacity(
+            opacity: opacity,
+            duration: UX.transitionDuration,
+            child: IconButton(
+              padding: EdgeInsets.all(0),
+              constraints: BoxConstraints(
+                maxWidth: 20,
+                maxHeight: 20,
+              ),
+              icon: GeniusIcon(),
+              onPressed: openUrl,
             ),
-            icon: GeniusIcon(),
-            onPressed: openUrl,
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }

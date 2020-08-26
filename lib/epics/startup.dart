3ea4bd09199fa0f863b0db0212de41cd76aa4686
hobroker <igor.leahu24@gmail.com>
2020-08-26 21:32:04 +0300

    Update .circle

--- lib/epics/startup.dart
+++ lib/epics/startup.dart
@@ -3,7 +3,6 @@ import 'package:flyrics/actions/player_actions.dart';
 import 'package:flyrics/actions/track_actions.dart';
 import 'package:flyrics/api/api.dart';
 import 'package:flyrics/models/state/app_state.dart';
-import 'package:flyrics/selectors/player.dart';
 import 'package:redux_epics/redux_epics.dart';
 
 Stream fetchTrackOnStartEpic(Stream actions, store) => actions

78e0bfa89dad39431384296b22b02c2900bc6142
hobroker <igor.leahu24@gmail.com>
2020-08-27 12:53:39 +0300

    Update github ci workflow

--- scripts/setup_flutter.sh
+++ scripts/setup_flutter.sh
@@ -3,7 +3,8 @@
 set -e
 
 git clone https://github.com/flutter/flutter.git -b dev --depth 1
-echo "::set-env name=PATH::$PATH:`pwd`/flutter/bin"
+echo "::set-env name=PATH::$PATH:$(pwd)/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 flutter config --enable-macos-desktop
 flutter pub get
 touch .env

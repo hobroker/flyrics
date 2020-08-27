2c310abca86d56b60cef403baa079ed67d998e1e
hobroker <igor.leahu24@gmail.com>
2020-08-27 13:14:30 +0300

    Add logs to setup_flutter.sh

--- scripts/setup_flutter.sh
+++ scripts/setup_flutter.sh
@@ -4,6 +4,7 @@ set -e
 
 PROJECT_PATH=$(pwd)
 
+echo "> clone flutter"
 cd ..
 git clone https://github.com/flutter/flutter.git -b dev --depth 1
 NEW_PATH="$PATH:$(pwd)/flutter/bin"
@@ -12,5 +13,8 @@ export PATH="$NEW_PATH"
 
 cd "${PROJECT_PATH}"
 
+echo "> run pub get"
 flutter pub get
+
+echo "> create .env file"
 touch .env

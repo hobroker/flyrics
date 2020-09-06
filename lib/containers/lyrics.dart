019d4d14005f80e4c9c4a6ca41c920019adffa5d
hobroker <igor.leahu24@gmail.com>
2020-09-06 23:08:44 +0300

    Remove Conditional.single usages

--- lib/containers/lyrics.dart
+++ lib/containers/lyrics.dart
@@ -4,7 +4,6 @@ import 'package:flutter_redux_hooks/flutter_redux_hooks.dart';
 import 'package:flyrics/models/state/app_state.dart';
 import 'package:flyrics/selectors/artwork.dart';
 import 'package:flyrics/selectors/lyrics.dart';
-import 'package:flyrics/utils/conditional.dart';
 import 'package:flyrics/views/lyrics/lyrics_placeholder.dart';
 import 'package:flyrics/views/lyrics/lyrics_screen.dart';
 
@@ -16,15 +15,15 @@ class Lyrics extends HookWidget {
     final hasLyrics = useSelector<AppState, bool>(activeTrackHasLyrics);
     final isLoading = useSelector<AppState, bool>(areLyricsLoading);
 
-    return Conditional.single(
-      when: hasLyrics,
-      render: () => LyricsScreen(
+    if (hasLyrics) {
+      return LyricsScreen(
         textColor: textColor,
         text: text,
-      ),
-      fallback: () => LyricsPlaceholder(
-        isAnimated: isLoading,
-      ),
+      );
+    }
+
+    return LyricsPlaceholder(
+      isAnimated: isLoading,
     );
   }
 }

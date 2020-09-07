4cf7bb8a785ba9dc6d4f4f0d2789c38584f55d58
hobroker <igor.leahu24@gmail.com>
2020-09-07 07:00:00 +0300

    Add AppLogger

--- lib/modules/locator.dart
+++ lib/modules/locator.dart
@@ -1,5 +1,6 @@
 import 'package:flyrics/api/api.dart';
 import 'package:flyrics/api/shell_api.dart';
+import 'package:flyrics/modules/app_logger.dart';
 import 'package:flyrics/modules/config.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,4 +11,5 @@ Future setupLocator() async {
 
   I.registerSingleton<Api>(Api(config));
   I.registerSingleton<Terminal>(Terminal());
+  I.registerSingleton<AppLogger>(AppLogger());
 }

87e828f18e9eb50a32fd1f52e757339a40bcf5f5
hobroker <igor.leahu24@gmail.com>
2020-09-08 07:34:00 +0300

    Adjust TrackDetails

--- lib/views/header.dart
+++ lib/views/header.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flyrics/views/artwork/artwork.dart';
 import 'package:flyrics/views/track/track_details.dart';
-import 'package:flyrics/views/track/track_wrapper.dart';
 
 class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        TrackWrapper(
-          child: TrackDetails(),
-        ),
-        Artwork(),
-      ],
-    );
+    return Row(children: [TrackDetails(), Artwork()]);
   }
 }
--- lib/views/track/track_details.dart
+++ lib/views/track/track_details.dart
@@ -4,12 +4,15 @@ import 'package:flyrics/hooks/store.dart';
 import 'package:flyrics/selectors/track.dart';
 import 'package:flyrics/views/track/track_placeholder.dart';
 import 'package:flyrics/views/track/track_screen.dart';
+import 'package:flyrics/views/track/track_wrapper.dart';
 
 class TrackDetails extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final trackExists = useSelector(hasTrack);
 
-    return trackExists ? TrackScreen() : TrackPlaceholder();
+    return TrackWrapper(
+      child: trackExists ? TrackScreen() : TrackPlaceholder(),
+    );
   }
 }

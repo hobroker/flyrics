143f79d2fa777f24abb0c83526b4fab87aa7a644
hobroker <igor.leahu24@gmail.com>
2020-09-12 02:38:58 +0300

    Adjust locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flyrics/api/api.dart';
 import 'package:flyrics/containers/app.dart';
 import 'package:flyrics/modules/locator.dart';
+import 'package:flyrics/stores/player.dart';
+
+import 'api/config.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final config = await ConfigService.init();
+  final api = Api(config);
+  final player = PlayerStore();
 
-  await setupLocator();
+  await setupLocator(
+    api: api,
+    player: player,
+  );
 
   runApp(App());
 }

da463704eae856cdaa77086ddf2c7010a0dc5217
hobroker <igor.leahu24@gmail.com>
2020-09-12 03:44:57 +0300

    Remove map_list

--- lib/views/map_list.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-List mapList<T>(List list, Widget Function(dynamic, int) builder) {
-  var idx = 0;
-
-  return list.map((item) => builder(item, idx++)).toList(growable: false);
-}

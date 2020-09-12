cc28eab4655a6a0e6575030d0a1b3b1240af2732
hobroker <igor.leahu24@gmail.com>
2020-09-12 18:38:58 +0300

    Adjust locator

--- lib/modules/locator.dart
+++ lib/modules/locator.dart
@@ -1,5 +1,4 @@
 import 'package:flyrics/api/api.dart';
-import 'package:flyrics/api/config.dart';
 import 'package:flyrics/api/genius.dart';
 import 'package:flyrics/api/spotify.dart';
 import 'package:flyrics/api/terminal.dart';
@@ -10,26 +9,19 @@ import 'package:get_it/get_it.dart';
 
 final I = GetIt.asNewInstance();
 
-Future setupLocator() async {
+Future setupLocator({
+  Api api,
+  PlayerStore player,
+}) async {
   I.registerSingleton<AppLogger>(AppLogger());
 
-  await _setupApi();
-  _setupMobx();
-}
-
-void _setupApi() async {
-  final config = await ConfigService.init();
-  final api = Api(config);
-
+  // api
   I.registerSingleton<Api>(api);
   I.registerSingleton<TerminalService>(api.terminal);
   I.registerSingleton<SpotifyService>(api.spotify);
   I.registerSingleton<GeniusService>(api.genius);
-}
 
-void _setupMobx() {
-  final player = PlayerStore();
-
+  //mobx
   I.registerSingleton<PlayerStore>(player);
   I.registerSingleton<TrackStore>(player.track);
 }

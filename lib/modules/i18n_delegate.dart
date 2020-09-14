27d244a5b855b94fcf8ad9c481aed1db875c420a
hobroker <igor.leahu24@gmail.com>
2020-09-14 20:30:42 +0300

    Adjust i18nDelegate

--- lib/modules/i18n_delegate.dart
+++ lib/modules/i18n_delegate.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flyrics/constants/asset_constants.dart';
 import 'package:flyrics/utils/debug.dart';
 
 final i18nDelegate = FlutterI18nDelegate(
   translationLoader: FileTranslationLoader(
-    fallbackFile: 'en',
     basePath: AssetConstants.i18nBasePath,
-    forcedLocale: Locale('en'),
   ),
   missingTranslationHandler: (key, locale) {
     var code = locale.languageCode;

d099931191fbae5f0146be23198b0aee3cf1c50a
hobroker <igor.leahu24@gmail.com>
2020-09-19 22:23:06 +0300

    Fix analysis

--- lib/views/track/track_details.dart
+++ lib/views/track/track_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flyrics/views/track/track_artist.dart';
 import 'package:flyrics/views/track/track_name.dart';
 import 'package:flyrics/views/track/track_wrapper.dart';

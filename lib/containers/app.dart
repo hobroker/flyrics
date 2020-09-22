d6ee07430a9d2ad9420aa9a4a9e147bd3f4c5dce
hobroker <igor.leahu24@gmail.com>
2020-09-22 09:08:44 +0300

    Remove dynamic theme from MaterialApp
    It's not necessary and it rebuilds the MaterialApp on changes

--- lib/containers/app.dart
+++ lib/containers/app.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flyrics/containers/home_page.dart';
-import 'package:flyrics/hooks/injections.dart';
 import 'package:flyrics/modules/i18n_delegate.dart';
-import 'package:flyrics/modules/mobx/o.dart';
+import 'package:flyrics/modules/theme/default_theme.dart';
 
-class App extends HookWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ux = useUX();
-
-    return O(
-      () => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: [i18nDelegate],
-        supportedLocales: [Locale('en', '')],
-        builder: FlutterI18n.rootAppBuilder(),
-        home: HomePage(),
-        theme: ux.theme,
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [i18nDelegate],
+      supportedLocales: [Locale('en', '')],
+      builder: FlutterI18n.rootAppBuilder(),
+      home: HomePage(),
+      theme: defaultTheme,
     );
   }
 }
